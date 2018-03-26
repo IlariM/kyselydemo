3f8ff43adbc3d000e87f51f4f2f5f425c64e9619
Munukka Ilari <a1700375@myy.haaga-helia.fi>
2018-03-26 14:59:18 +0300

    first commit

---
+++ src/main/java/com/kevatsaapas/kyselydemo/domain/KysymysRepository.java
@@ -0,0 +1,7 @@
+package com.kevatsaapas.kyselydemo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KysymysRepository extends CrudRepository <Kysymys, Long>{
+
+}
